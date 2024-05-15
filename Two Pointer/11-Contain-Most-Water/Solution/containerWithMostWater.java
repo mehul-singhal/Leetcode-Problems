12e77d7d6870b82572ad283b06832e1523e8c775
mehul-singhal <mehulbtp@gmail.com>
2024-05-16 02:34:33 +0530

    Comments for question 11

--- Two Pointer/11-Contain-Most-Water/Solution/containerWithMostWater.java
+++ Two Pointer/11-Contain-Most-Water/Solution/containerWithMostWater.java
@@ -1,13 +1,23 @@
 public class containerWithMostWater {
     public int maxArea(int[] height) {
+        //This is classic two pointer question
+        //The thought process is basically
+        //To find a pair of values which multiplied by the distance b/w them is maximum
         int max = 0;
-        int lp = 0; 
+        //Starting the left side pointer
+        int lp = 0;
+        //Starting the right side pointer 
         int rp = height.length-1;
 
+        //Traversing to meet the pointers in the middle
         while(lp<rp){
+            //Checking which among left and right pointer value is less
+            //Multiplying the min value with the distance b/w these two values
+            //Now checking if the new value in each iteration is more than the already set max value.
             if((Math.min(height[lp], height[rp])*(rp-lp)) > max){
                 max = Math.min(height[lp], height[rp])*(rp-lp);
             }
+            //The pointer with less value moves to the next one.
             if(height[lp]<height[rp]){
                 lp++;
             }else{
