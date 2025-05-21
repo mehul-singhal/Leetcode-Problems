2357305fdf0fb1106f91f20eba492dfe8784ed07
mehul-singhal <mehulbtp@gmail.com>
2025-05-21 18:21:41 +0530

    Removed a random user's solution

--- Arrays/349-Intersection-of-arrays/intersection.java
+++ Arrays/349-Intersection-of-arrays/intersection.java
@@ -43,35 +43,6 @@ public class intersection {
         return result;
     }
 
-    public int[] intersectionBest(int[] nums1, int[] nums2) {
-        int[] counter = new int[1001];
-        for(int num : nums1)
-        {
-            counter[num] = 1;
-        }
-        int count=0;
-        for(int num : nums2)
-        {
-            if(counter[num] == 1)
-            {
-                counter[num] = 2;
-                count++;
-            }
-        }
-        int[] res = new int[count];
-        int ind=0;
-        for(int i : nums2)
-        {
-            if(counter[i] == 2)
-            {
-                res[ind++] = i;
-                counter[i] = 1;
-            }
-        }
-        return res;
-        
-    }
-
     public static void main(String args[]){
         intersection obj = new intersection();
         int[] nums1 = {1, 2, 2, 1};
