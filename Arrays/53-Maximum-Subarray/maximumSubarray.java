5d0e025d16e46fc67996bd07f2cadfe88e89608b
mehul-singhal <mehulbtp@gmail.com>
2025-06-02 23:56:26 +0530

    Maximum Subarray Sum

--- Arrays/53-Maximum-Subarray/maximumSubarray.java
+++ Arrays/53-Maximum-Subarray/maximumSubarray.java
@@ -10,5 +10,42 @@ public class maximumSubarray {
             maxSub = Math.max(currentSum, maxSub);
         }
         return maxSub;
-    }    
+    }
+
+    public static void maxSubArrayWithPrint(int[] arr) {
+        int n = arr.length;
+        int currentSum = arr[0];
+        int totalSum = arr[0];
+        int start = 0, resEnd = 0, resStart = 0;
+        for(int i = 1; i<n; i++){
+            // currentSum = Math.max((arr[i]+currentSum),arr[i]);
+            // totalSum = Math.max(currentSum, totalSum);
+            if(currentSum+arr[i] > arr[i]){
+                currentSum += arr[i];
+            }
+            else {
+                start = i;
+                currentSum = arr[i];
+            }
+
+            if(currentSum > totalSum){
+                totalSum = currentSum;
+                resStart = start; 
+                resEnd = i;
+            }
+        }
+
+        for(int i = resStart; i<=resEnd; i++){
+            System.out.println(arr[i]);
+        }   
+    }
+    
+    public static void main(String[] args) {
+    int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int maxSum = maximumSumOptimal(arr);
+    System.out.println("Maximum Subarray Sum: " + maxSum);
+    
+    System.out.println("Maximum Subarray Elements:");
+    maxSubArrayWithPrint(arr);
+}
 }
