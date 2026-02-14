ddbead6197b336375f9eaa0f2309aeb7dcd00e1e
mehul-singhal <mehul-singhal@outlook.com>
2026-02-14 23:18:39 +0530

    Added Path Sum

---
+++ Tree/BinaryTreeNode.java
@@ -0,0 +1,11 @@
+package Tree;
+public class BinaryTreeNode {
+    public int data;
+    public BinaryTreeNode right, left;
+
+    public BinaryTreeNode(int data){
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
